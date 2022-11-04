0b7bcafdc75938c249f89852e44a2999fe6771e3
Aman <eyeon4840@gmail.com>
2022-11-04 18:29:04 +0530

    lambda function.

--- src/main/kotlin/Nov_4.kt
+++ src/main/kotlin/Nov_4.kt
@@ -2,11 +2,30 @@ class Nov_4 {
 
     companion object {
         val upperCase1: (String) -> String = {str: String -> str.uppercase()}
+
+        val upperCase2= { str: String -> doit(str) }    // Type inference inside lambda  ( compiler automatically guessed it's String)
+        private fun doit(str: String): String {
+            return "wola " + str
+        }
+
+        // val upperCase4 = { str -> str.uppercase() }      // can't do both , Type inference , inside & outside
+
+        val upperCase3 = {str: String -> str.uppercase()}       // Type inference outside lambda
+
+        val upperCase5 : (String) -> String = { it.uppercase()}     // For lambdas with a single parameter, can use it
+
+        val upperCase6 : (String) -> String = String::uppercase     // if your lambda consists of a single function call, you may use function pointers (::) .
+
+
     }
 
 
 }
 fun main() {
    println( Nov_4.upperCase1("hello"))
+   println( Nov_4.upperCase2("hello2"))
+   println( Nov_4.upperCase3("hello3"))
+   println( Nov_4.upperCase5("hello5"))
+   println( Nov_4.upperCase6("hello6"))
 
 }
