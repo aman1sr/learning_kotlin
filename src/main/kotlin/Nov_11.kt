af22d3de2ffecdda0decfc3864d0a7631a4d2028
Aman <eyeon4840@gmail.com>
2022-11-11 19:04:28 +0530

    KOTLIN - collections

--- src/main/kotlin/Nov_11.kt
+++ src/main/kotlin/Nov_11.kt
@@ -21,6 +21,16 @@
 //      A map is a collection of key/value pairs
 //          where each key is unique and is used to retrieve the corresponding value
 
+private val EZPassAccoutns: MutableMap<Int, Int> = mutableMapOf(1 to 100, 2 to 220, 3 to 330)
+private val EZPassReport : Map<Int, Int> = EZPassAccoutns
+
+fun accountReport() {
+    println("report:::")
+    EZPassReport.forEach{
+        k,v -> println("ID: $k , credit: $v")
+    }
+}
+
 
 
 /*-----------------------------------Function Scope---------------------------------------*/
@@ -28,7 +38,11 @@
 
     fun main() {
 
+        /* LIST */
     addStuff(4)
         println(getList())
+
+        /* MAP */
+        accountReport()
 
 }
